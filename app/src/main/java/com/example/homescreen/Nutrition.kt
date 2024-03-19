e4aaaabbc8dd9cd2d7893f19d5895d429d48b79d
tyler <minhhieu.ha.2520@gmail.com>
2024-03-19 18:18:48 +1100

    [REF] Remove some unncessary code

--- app/src/main/java/com/example/homescreen/Nutrition.kt
+++ app/src/main/java/com/example/homescreen/Nutrition.kt
@@ -3,7 +3,6 @@ package com.example.homescreen
 import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -27,9 +26,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
