19880dd626cd5f9144a060e71b0286ccaf21a701
tyler <minhhieu.ha.2520@gmail.com>
2024-03-19 18:17:59 +1100

    [IMP] Improve Nutrition View

--- app/src/main/java/com/example/homescreen/Nutrition.kt
+++ app/src/main/java/com/example/homescreen/Nutrition.kt
@@ -3,14 +3,13 @@ package com.example.homescreen
 import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
@@ -28,165 +27,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
 import coil.compose.rememberImagePainter
-
-@Composable
-fun NutritionTrackerScreen() {
-    var foodName by remember { mutableStateOf("") }
-    var calories by remember { mutableStateOf("") }
-    var protein by remember { mutableStateOf("") }
-    var carbs by remember { mutableStateOf("") }
-    var fats by remember { mutableStateOf("") }
-
-    val context = LocalContext.current
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState())
-    ) {
-        Text(
-            text = "Log Food Intake",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        // Input fields for food log
-        OutlinedTextField(
-            value = foodName,
-            onValueChange = { foodName = it },
-            label = { Text("Food Name") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = calories,
-            onValueChange = { calories = it },
-            label = { Text("Calories") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            ),
-            keyboardActions = KeyboardActions(
-                onNext = {
-                    // Focus on the next field
-                }
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = protein,
-            onValueChange = { protein = it },
-            label = { Text("Protein (g)") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            ),
-            keyboardActions = KeyboardActions(
-                onNext = {
-                    // Focus on the next field
-                }
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = carbs,
-            onValueChange = { carbs = it },
-            label = { Text("Carbs (g)") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            ),
-            keyboardActions = KeyboardActions(
-                onNext = {
-                    // Focus on the next field
-                }
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = fats,
-            onValueChange = { fats = it },
-            label = { Text("Fats (g)") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = {
-                    // Hide the keyboard
-                }
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Save button
-        Button(
-            onClick = {
-                // Save food log to database or perform desired action
-                Toast.makeText(context, "Food Logged: $foodName", Toast.LENGTH_SHORT).show()
-            },
-        ) {
-            Text(text = "Save")
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "Monitor Nutritional Goals",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        // Placeholder for nutritional goals monitoring
-        Text(
-            text = "Your Nutritional Goals will appear here.",
-            modifier = Modifier.fillMaxWidth(),
-            color = Color.Gray
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "Share Recipes or Dietary Tips",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        // Placeholder for sharing recipes or tips
-        Text(
-            text = "Share your favorite recipes or dietary tips with the community.",
-            modifier = Modifier.fillMaxWidth(),
-            color = Color.Gray
-        )
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -214,19 +67,305 @@ fun NutritionTracker() {
             )
         }
     ) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 64.dp)
+        ) {
+            NutritionFormView()
+
+        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
                 .padding(top = 64.dp)
         ) {
-            FoodList(foods = foods) {}
-            if (showForm) {
-                NutritionTrackerScreen()
+//            if (showForm) {
+//                CreateNutritionForm()
+//            }
+//            FoodList(foods = foods) {}
+        }
+    }
+}
+
+
+@Composable
+fun NutritionFormView() {
+    val foodName by remember { mutableStateOf("Banana") }
+    val calories by remember { mutableStateOf(103) }
+    val description by remember {mutableStateOf("The banana (Musa genus) is a remarkable fruit, cherished across the globe for its flavor, nutritional value, and year-round availability.")}
+    val protein by remember { mutableStateOf(0) }
+    val carbs by remember { mutableStateOf(0) }
+    val fats by remember { mutableStateOf(0) }
+
+    Image(painter = painterResource(R.drawable.banana), contentDescription = "Food Image")
+
+    Row {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+
+            Text(
+                text = foodName,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            if (description != "") {
+                Text(
+                    text = "The banana (Musa genus) is a remarkable fruit, cherished across the globe for its flavor, nutritional value, and year-round availability.",
+                    textAlign = TextAlign.Justify,
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Card{
+                Column(Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Protein",
+                        )
+                        Text(
+                            text = "$protein g",
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Calories",
+                        )
+                        Text(
+                            text = "$calories kJ",
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Carbs",
+                        )
+                        Text(
+                            text = "$carbs g",
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Fats",
+                        )
+                        Text(
+                            text = "$fats g",
+                        )
+                    }
+
+//                    Text(
+//                        text = "Protein: $protein g",
+//                        fontSize = 16.sp,
+//                    )
+//
+//                    Text(
+//                        text = "Carbs: $carbs g",
+//                        fontSize = 16.sp,
+//                    )
+//
+//                    Text(
+//                        text = "Fats: $fats g",
+//                        fontSize = 16.sp,
+//                    )
+                }
+            }
+
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+
+@Composable
+fun CreateNutritionForm() {
+    var foodName by remember { mutableStateOf("") }
+    var calories by remember { mutableStateOf("") }
+    var protein by remember { mutableStateOf("") }
+    var carbs by remember { mutableStateOf("") }
+    var fats by remember { mutableStateOf("") }
+    var imageUrl by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Add New Food",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        Row {
+            // Image preview
+            Image(
+                painter = rememberImagePainter(imageUrl),
+                contentDescription = "Food Image",
+                modifier = Modifier
+                    .size(128.dp)
+                    .padding(end = 16.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            // Input fields for food attributes
+            Column {
+                OutlinedTextField(
+                    value = foodName,
+                    onValueChange = { foodName = it },
+                    label = { Text("Food Name") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+//                OutlinedTextField(
+//                    value = imageUrl,
+//                    onValueChange = { imageUrl = it },
+//                    label = { Text("Image URL") },
+//                    modifier = Modifier.fillMaxWidth()
+//                )
+//
+//                Spacer(modifier = Modifier.height(8.dp))
+            }
+
+        }
+        Column {
+            OutlinedTextField(
+                value = calories,
+                onValueChange = { calories = it },
+                label = { Text("Calories") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        // Focus on the next field
+                    }
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = protein,
+                onValueChange = { protein = it },
+                label = { Text("Protein (g)") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        // Focus on the next field
+                    }
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = carbs,
+                onValueChange = { carbs = it },
+                label = { Text("Carbs (g)") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        // Focus on the next field
+                    }
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = fats,
+                onValueChange = { fats = it },
+                label = { Text("Fats (g)") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        // Hide the keyboard
+                    }
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Save button
+            Button(
+                onClick = {
+                    // Save food log to database or perform desired action
+                    val newFood = Food(
+                        id = System.currentTimeMillis(),
+                        name = foodName,
+                        imageUrl = imageUrl,
+                        calories = calories.toIntOrNull() ?: 0,
+                        protein = protein.toFloatOrNull() ?: 0f,
+                        carbs = carbs.toFloatOrNull() ?: 0f,
+                        fats = fats.toFloatOrNull() ?: 0f
+                    )
+                    foodName = ""
+                    calories = ""
+                    protein = ""
+                    carbs = ""
+                    fats = ""
+                    imageUrl = ""
+                    Toast.makeText(context, "Food Logged: $foodName", Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Add Food")
             }
         }
     }
 }
+
 
 @Composable
 fun FoodList(foods: List<Food>, onFoodClick: (Food) -> Unit) {
