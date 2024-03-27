beb016326330aef024dfe016aa09647ac181d6f5
alice856262 <alice856262@gmail.com>
2024-03-27 13:07:19 +1100

    Change theme and color

--- app/src/main/java/com/example/homescreen/ui/theme/Color.kt
+++ app/src/main/java/com/example/homescreen/ui/theme/Color.kt
@@ -6,6 +6,6 @@ val Purple80 = Color(0xFFBDBDBD)
 val PurpleGrey80 = Color(0xFF7B9FCC)
 val Pink80 = Color(0xFFFFF59D)
 
-val Purple40 = Color(0xFFFF9878)
-val PurpleGrey40 = Color(0xFF5472D3)
+val Purple40 = Color(0xFF5472D3)
+val PurpleGrey40 = Color(0xFFFF9878)
 val Pink40 = Color(0xFFFBC02D)
