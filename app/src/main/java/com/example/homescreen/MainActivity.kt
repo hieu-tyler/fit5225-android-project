8f3c5ef23e057a9d23c7ed8e8c9a9f6283c56d8d
davaabaa <163719445+davaabaa@users.noreply.github.com>
2024-05-09 10:27:28 +1000

    Update MainActivity.kt

--- app/src/main/java/com/example/homescreen/MainActivity.kt
+++ app/src/main/java/com/example/homescreen/MainActivity.kt
@@ -1,17 +1,10 @@
 package com.example.homescreen
 
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.core.app.ActivityCompat
 import com.example.homescreen.ui.theme.HomeScreenTheme
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.mapbox.common.location.Location
-import com.mapbox.geojson.Point
 
 class MainActivity : ComponentActivity() {
 
