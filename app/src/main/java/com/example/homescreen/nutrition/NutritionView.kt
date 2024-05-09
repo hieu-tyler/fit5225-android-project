c57be9157bc6fea629497c69a2837fc1b82e2778
hieu-tyler <minhhieu.ha.2520@gmail.com>
2024-05-09 14:57:43 +1000

    [IMP] Improve

--- app/src/main/java/com/example/homescreen/nutrition/NutritionView.kt
+++ app/src/main/java/com/example/homescreen/nutrition/NutritionView.kt
@@ -66,9 +66,7 @@ fun NutritionFormView(navController: NavController, food: Food) {
     val description by remember { mutableStateOf(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
                 "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
-                "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum" +
-                " dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
-                "deserunt mollit anim id est laborum."
+                "laboris nisi ut aliquip ex ea commodo consequat."
     ) }
     val context = LocalContext.current
     val resourceId: Int = context.resources.getIdentifier(food.imageUrl, "drawable", context.packageName)
